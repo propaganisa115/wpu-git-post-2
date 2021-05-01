3a717135c288f7d5a4891f8fab6613e345b52601
propaganisa115 <propaganisa.reg@gmail.com>
2021-05-02 01:13:27 +0700

    edit index file

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     coba git 
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum laborum quis quia impedit voluptas nobis, optio quas nulla tempora officiis quo, saepe repellat cumque quod perspiciatis. Fuga voluptate quibusdam illum.</p>
+    <ul>
+    
+    <li>list 1</li>
+    </ul>
 </body>
 </html>
